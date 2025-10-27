480ba1f120e620b8ea360f52fe35633061707271
Bykevin10 <pintokevin243@gmail.com>
2025-10-27 16:37:50 -0500

    develop: despliegue a gh-pagies

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ export default function App() {
 
   return (
     <div className="max-w-md mx-auto mt-10 p-2  rounded shadow">
-      <h1 className="text-3xl font-bold mb-5 text-center">TODO LIST APP</h1>
+      <h1 className="text-3xl font-bold mb-5 text-center">APP_TODO_LIST.</h1>
       <div className="flex gap-3 mb-5">
         <input className="flex-1 p-2 border rounded" type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Añadir Tarea" />
         <button className="bg-blue-500 text-white px-4 p-y-2 rounded" onClick={agregarTarea} >Añadir Tareas</button>
